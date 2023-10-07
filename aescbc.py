9a30c50a15ba1c575b5bac9314c25abed40852f1
Zhi Yang <tohzhiyangrv@gmail.com>
2023-10-07 12:26:05 +0800

    Added encoding of key and IV to bytes

--- aescbc.py
+++ aescbc.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import sys,os
+import Crypto
 from Crypto.Cipher import AES
 from settings import *
 mode = AES.MODE_CBC
+
+key = '0123456789abcdef'
+IV='0123456789abcdef'
+
 
 # PKCS7 padding: adding b bytes each worth b 
 def padding(text):
@@ -15,13 +20,15 @@ def unpadding(data):
 
 # AES CBC Encryption
 def encryption(text):
-    encryptor = AES.new(key, mode, IV=IV)
+    # TODO: Have to encode the key and IV to byte before passing into AES encrytion object
+    encryptor = AES.new(key.encode('utf-8'), mode,IV=IV.encode('utf-8'))
     padded_text = padding(text)
-    return encryptor.encrypt(padded_text)
+    return encryptor.encrypt(padded_text.encode('utf-8'))
 
 # AES CBC decryption without padding
 def decryption(encrypted):
-    decryptor = AES.new(key, mode, IV=IV)
+    # TODO: Have to encode the key and IV to byte before passing into AES decryption object
+    decryptor = AES.new(key.encode('utf-8'), mode,IV=IV.encode('utf-8'))
     return decryptor.decrypt(encrypted)
 
 
@@ -42,7 +49,7 @@ def pkcs7_padding(data):
 
 usage = """
 Usage:
-  python3 paescbc.py <message>         encrypts and displays the message (output in hex format)
+  python3 aescbc.py <message>         encrypts and displays the message (output in hex format)
   python3 aescbc.py -d <hex code>      decrypts and displays the message
 
 Cryptographic parameters can be changed in settings.py
